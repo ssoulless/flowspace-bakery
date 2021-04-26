1613fd2fd9034e0c80d91c29556efe67b6d0735c
Sebastian Velandia <sebas.velandia27@gmail.com>
2021-04-26 17:28:52 -0500

    Moved filling message check to a helper method

--- app/helpers/cookies_helper.rb
+++ app/helpers/cookies_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# Returns 'no fillings' when cookie filling is empty
 module CookiesHelper
+  def get_filling_message(cookie)
+    cookie.fillings.strip.empty? ? 'no fillings' : cookie.fillings
+  end
 end
